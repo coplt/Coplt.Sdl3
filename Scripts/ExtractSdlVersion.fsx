45e0666f79f6c6c7a7ad9ecce84db5e90d026c83
U2A5F <U2A5F@outlook.com>
2025-04-23 13:46:29 +0800

    win64 nuget

---
+++ Scripts/ExtractSdlVersion.fsx
@@ -0,0 +1,25 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+let VersionRegex = Regex("\"([^\"]+)\"")
+
+let version =
+    VersionRegex
+        .Match(
+            File.ReadLines("./SDL/CMakeLists.txt")
+            |> Seq.where (fun l -> l.StartsWith "project(SDL3")
+            |> Seq.head
+        )
+        .Groups[1]
+        .Value
+
+let suffix = File.ReadAllText("./VersionSuffix.txt").Trim();
+
+let output = $"<Project>
+    <PropertyGroup>
+        <Version>{version}{suffix}</Version>
+    </PropertyGroup>
+</Project>
+"
+
+File.WriteAllText("./Version.props", output)
