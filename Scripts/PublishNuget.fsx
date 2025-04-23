115831b7ad1739761476e316481692ee27f5f201
U2A5F <U2A5F@outlook.com>
2025-04-23 16:49:30 +0800

    publish

---
+++ Scripts/PublishNuget.fsx
@@ -0,0 +1,22 @@
+﻿open System
+open System.Diagnostics
+open System.IO
+
+let mutable NUGET_KEY = Environment.GetEnvironmentVariable("NUGET_KEY")
+
+if NUGET_KEY = null then
+    NUGET_KEY <- ""
+
+for path in Directory.GetFiles "./artifact" do
+    Process
+        .Start(
+            "dotnet",
+            [| "nuget"
+               "push"
+               path
+               "--api-key"
+               NUGET_KEY
+               "--source"
+               "https://api.nuget.org/v3/index.json" |]
+        )
+        .WaitForExit()
